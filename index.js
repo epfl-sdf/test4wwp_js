dc4f6331af9eace0d8676c74e379c8d0ea97216a
obieler <olivier.bieler@epfl.ch>
2017-07-21 16:36:53 +0200

    iframe height to 95vh

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var querystring = require('querystring');
 var zHeader = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" ' +
              '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html>' +
              '<head><meta charset="utf-8"><title>Console de comparaison visuelle V1.0</title></head>' +
-             '<body style="margin:0px;padding:0px;">';
+             '<body style="margin:0px;padding:0px;overflow:hidden">';
 
 app.use(express.static(path.join(__dirname, './public')));
 
@@ -17,8 +17,8 @@ app.get('/', function(req, res) {
   });
   res.write(zHeader);
   if ('url1' in params && 'url2' in params) {
-    var iframe1 = '<iframe id="frame1" src="' + params['url1'] + '" width="49%" height="1000" align="left" scrolling="yes"></iframe>';
-    var iframe2 = '<iframe id="frame2" src="' + params['url2'] + '" width="49%" height="1000" align="right" scrolling="yes"></iframe>';
+    var iframe1 = '<iframe id="frame1" src="' + params['url1'] + '" width="49%" style="height:95vh" align="left" scrolling="yes"></iframe>';
+    var iframe2 = '<iframe id="frame2" src="' + params['url2'] + '" width="49%" style="height:95vh" align="right" scrolling="yes"></iframe>';
     res.write(iframe1);
     res.write(iframe2);
     res.write('<script src="iframes_sync_scrolling.js"></script>');
